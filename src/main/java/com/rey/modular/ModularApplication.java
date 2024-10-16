656e22dd72ecd7b9f79b0beee3886730094405bf
reypham <pea5137@gmail.com>
2024-10-16 11:12:44 +0700

    Correct exclude filter

--- src/main/java/com/rey/modular/ModularApplication.java
+++ src/main/java/com/rey/modular/ModularApplication.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "com.rey.modular.*.*.*"))
+@ComponentScan(excludeFilters = {
+		@ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "com.rey.modular..*.*.*")
+})
 public class ModularApplication {
 
 	public static void main(String[] args) {
