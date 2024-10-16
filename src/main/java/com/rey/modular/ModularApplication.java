002add580f846bdd76ba1c25eb1bdb2a12af503a
reypham <pea5137@gmail.com>
2024-10-16 10:48:13 +0700

    Exclude sub packages

--- src/main/java/com/rey/modular/ModularApplication.java
+++ src/main/java/com/rey/modular/ModularApplication.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "com.rey.modular.*.*.*"))
+@ComponentScan(excludeFilters = {
+		@ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "com.rey.modular.*.*.*"),
+		@ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "com.rey.modular.*.*.*.*")
+})
 public class ModularApplication {
 
 	public static void main(String[] args) {
